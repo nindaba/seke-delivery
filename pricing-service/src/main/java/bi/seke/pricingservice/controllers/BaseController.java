00c7c7f14b31dbdeaedd747e978c81c66c8b7b65
Nindaba <arthurninda@gmail.com>
2023-08-05 17:52:48 +0200

    #46 | added response status

--- pricing-service/src/main/java/bi/seke/pricingservice/controllers/BaseController.java
+++ pricing-service/src/main/java/bi/seke/pricingservice/controllers/BaseController.java
@@ -5,9 +5,11 @@ import bi.seke.schema.common.ErrorResponseDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class BaseController {
     @ExceptionHandler({PriceNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponseDTO handlePriceNotFound(final HttpServletRequest request, final PriceNotFoundException ex) {
         return new ErrorResponseDTO(
                 HttpStatus.NOT_FOUND.value(),
