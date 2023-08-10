106e7b9f8c9d6612fc4511cc07eaeb6bb4e08c2b
nindaba <86868096+nindaba@users.noreply.github.com>
2023-08-10 18:56:52 +0200

    #59 | #53 | PYS | set-up the service

---
+++ payment-service/src/test/java/bi/seke/paymentservice/PaymentServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package bi.seke.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PaymentServiceApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
